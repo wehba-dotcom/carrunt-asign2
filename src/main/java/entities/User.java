068877e43bb7892226501d5e0f94acb8a33f5653
wehba <Lenovo@wehba.com>
2021-11-10 10:53:41 +0100

    make all requestes Parallel from project to others APIs

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -47,6 +47,7 @@ public class User implements Serializable {
     return rolesAsStrings;
   }
   public User() {}
+  
   //TODO Change when password is hashed
    public boolean verifyPassword(String pw){
         return BCrypt.checkpw(pw,userPass);
